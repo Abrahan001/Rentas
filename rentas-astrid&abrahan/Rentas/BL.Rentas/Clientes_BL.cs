410b5a595f6181ef8148715e48a263ab5a92f718
Abrahan001 <agurciaa@gmail.com>
2018-10-28 15:38:59 -0600

    Tarea

---
+++ rentas-astrid&abrahan/Rentas/BL.Rentas/Clientes_BL.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.Rentas
+{
+    public class Clientes_BL
+    {
+        Contexto _contexto;
+        public BindingList<Clientes> ListaClientes { get; set; }
+        public Clientes_BL()
+        {
+           _contexto = new Contexto();
+            ListaClientes = new BindingList<Clientes>();
+            
+
+        }
+
+     
+
+        public BindingList<Clientes> ObtenerClientes()
+        {
+            _contexto.Cliente.Load();
+            ListaClientes = _contexto.Cliente.Local.ToBindingList();
+            return ListaClientes;
+        }
+        public Resultado GuardarClientes(Clientes cliente)
+        {
+            var resultado = Validar(cliente);
+            if (resultado.Exitoso==false)
+            {
+                return resultado;
+            }
+            _contexto.SaveChanges();
+            resultado.Exitoso = true;
+            return resultado;
+        }
+        public void Agregarcliente()
+        {
+            var nuevoCliente = new Clientes();
+            ListaClientes.Add(nuevoCliente);
+
+        }
+        public bool EditarCliente(int id)
+        {
+
+            foreach (var clientess in ListaClientes)
+            {
+                if (clientess.Id == id)
+                {
+
+                   
+                    return true;
+                 
+    }
+            }
+            return false;
+        }
+   
+        public bool EliminarCliente(int id)
+        {
+            foreach (var clientess in ListaClientes)
+            {
+                if (clientess.Id==id)
+                {
+                    ListaClientes.Remove(clientess);
+                    _contexto.SaveChanges();
+                    return true;
+                }
+            }
+            return false;
+        }
+        private Resultado Validar(Clientes cliente)
+        {
+            var resultado = new Resultado();
+            resultado.Exitoso = true;
+            if (string.IsNullOrEmpty(cliente.Nombre)==true)
+            {
+                resultado.Mensaje = "Ingrese el Nombre";
+                resultado.Exitoso = false;
+            }
+            if (string.IsNullOrEmpty(cliente.Correo) == true)
+            {
+                resultado.Mensaje = "Ingrese el Correo Electronico";
+                resultado.Exitoso = false;
+            }
+            if (cliente.Telefono <= 0)
+            {
+                resultado.Mensaje = "Ingrese los 8 digitos de su numero telefonico";
+                resultado.Exitoso = false;
+            }
+            if (string.IsNullOrEmpty(cliente.Direccion) == true)
+            {
+                resultado.Mensaje = "Ingrese la Direccion";
+                resultado.Exitoso = false;
+            }
+
+            return resultado;
+        }
+    }
+ 
+       public class Clientes
+    {
+        public int Id { get; set; }
+        public string  Nombre{ get; set; }
+        public string Correo { get; set; }
+        public int Telefono { get; set; }
+        public string Direccion { get; set; }
+        public bool Activar { get; set; }
+    }
+    public class Resultado
+    {
+        public bool Exitoso { get; set; }
+        public string Mensaje { get; set; }
+    }
+
+} 
---
+++ rentas-astrid&abrahan/Rentas/Win.Rentas/FormClientes.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BL.Rentas;
+
+namespace Win.Rentas
+{
+    public partial class FormClientes : Form
+    {
+        Clientes_BL _cliente;
+        private void FormClientes_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public FormClientes()
+        {
+
+            InitializeComponent();
+            _cliente = new Clientes_BL();
+            listaClientesBindingSource.DataSource = _cliente.ObtenerClientes();
+        }
+
+
+        private void listaClientesBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            listaClientesBindingSource.EndEdit();
+            var cliente = (Clientes)listaClientesBindingSource.Current;
+            var resultado = _cliente.GuardarClientes(cliente);
+            if (resultado.Exitoso == true)
+            {
+                listaClientesBindingSource.ResetBindings(false);
+                DesahabilitarHablitarBotones(true);
+                MessageBox.Show("Cliente Agregado");
+            }
+            else
+            {
+                MessageBox.Show(resultado.Mensaje);
+            }
+        }
+
+        private void bindingNavigatorAddNewItem_Click(object sender, EventArgs e)
+        {
+            _cliente.Agregarcliente();
+            listaClientesBindingSource.MoveLast();
+            DesahabilitarHablitarBotones(false);
+        }
+
+        private void DesahabilitarHablitarBotones(bool valor)
+        {
+            bindingNavigatorMoveFirstItem.Enabled = valor;
+            bindingNavigatorMoveLastItem.Enabled = valor;
+            bindingNavigatorMovePreviousItem.Enabled = valor;
+            bindingNavigatorMoveNextItem.Enabled = valor;
+            bindingNavigatorPositionItem.Enabled = valor;
+
+            bindingNavigatorAddNewItem.Enabled = valor;
+            bindingNavigatorDeleteItem.Enabled = valor;
+            toolStripButton1cancelar.Visible = !valor;
+        }
+
+        private void bindingNavigatorDeleteItem_Click(object sender, EventArgs e)
+        {
+
+            if (idTextBox.Text != "")
+            {
+                var resultado = MessageBox.Show("Desea eliminar este registro?", "Eliminar", MessageBoxButtons.YesNo);
+                if (resultado == DialogResult.Yes)
+                {
+                    var id = Convert.ToInt32(idTextBox.Text);
+                    Eliminar(id);
+                }
+
+            }
+
+        }
+        private void Eliminar(int id)
+        {
+
+            var resultado = _cliente.EliminarCliente(id);
+
+            if (resultado == true)
+            {
+                listaClientesBindingSource.ResetBindings(false);
+            }
+            else
+            {
+                MessageBox.Show("Ocurrio un error al eliminar al Cliente");
+            }
+        }
+
+        private void toolStripButton1cancelar_Click(object sender, EventArgs e)
+        {
+            DesahabilitarHablitarBotones(true);
+            Eliminar(0);
+        }
+
+       public void editar_Click(object sender, EventArgs e)
+        {
+            if (idTextBox.Text != "")
+            {
+                var id = Convert.ToInt32(idTextBox.Text);
+                 var cliente = new Clientes();
+                var resul = _cliente.EditarCliente(id);
+
+                if (resul == true)
+                {
+                                
+                     cliente.Nombre = nombreTextBox.Text;
+                    cliente.Correo = correoTextBox.Text;
+                    cliente.Telefono = Convert.ToInt32(telefonoTextBox.Text);
+                    cliente.Direccion = direccionTextBox.Text;
+                   
+                    
+                    }
+                }
+
+            }
+            }
+        }
+    
+
